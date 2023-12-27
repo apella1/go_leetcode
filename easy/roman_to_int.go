2f62a912a4a2c49f0f34cfac9290ce0e200d2b06
apella1 <johncollons5@gmail.com>
2023-12-27 20:59:52 +0300

    fix roman to int looping bug

--- easy/roman_to_int.go
+++ easy/roman_to_int.go
@@ -1,7 +1,7 @@
 package easy
 
 func RomanToInteger(roman string) int {
-	var value int
+	value := 0
 	romanValues := map[string]int{
 		"I": 1,
 		"V": 5,
@@ -10,13 +10,17 @@ func RomanToInteger(roman string) int {
 		"C": 100,
 		"M": 1000,
 	}
-	// IV
-	for i, v := range roman {
-		if i+1 < len(roman) && (romanValues[string(v)] < romanValues[string(roman[i+1])]) {
+	for i := 0; i < len(roman); i++ {
+		if i+1 < len(roman) && romanValues[string(roman[i])] < romanValues[string(roman[i+1])] {
 			// todo: difference between string and rune and the default when iterating over a string
-			value -= romanValues[string(v)]
+			value -= romanValues[string(roman[i])]
+			continue
 		}
-		value += romanValues[string(v)]
+		value += romanValues[string(roman[i])]
 	}
 	return value
 }
+
+/*
+todo break, continue -> do multiple loop practice exercises
+*/
